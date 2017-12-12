05e39e6882ac01aad36d6fc1cbf6241dcb5b7c5d
jcho <ji-hoon.cho@stjude.org>
2017-12-12 11:27:08 -0600

    Unsupervised analysis: 1. PCA plot has been implemented

--- server.R
+++ server.R
@@ -1,73 +1,161 @@
 ## server.R
 library(readxl)
+library(ggplot2)
+source("R/statTest.R")
 
 function (input, output) {
   ## Load JUMP -q output file (either id_uni_pep_quan.xlsx or id_uni_prot_quan.xlsx)
-  inFile = reactive(input$inputFile)
-  inputData = eventReactive(inFile(), {
-    inFileName = inFile()$name
+  inputData = eventReactive(input$inputFile, {
+    inFileName = input$inputFile$name
     if (length(grep("pep", inFileName))) {
       tbl = read_excel(inFileName, skip = 4) # Peptide publication table
+      level = "peptide"
     } else {
       tbl = read_excel(inFileName, skip = 1) # Protein publication table
+      level = "protein"
     }
-    df = as.data.frame(tbl)
+    list(data = as.data.frame(tbl), level = level)
   })
 
-  ## Specificiation of sample groups
-  observeEvent(is.null(input$inputFile), {
-    inputNumberGroups = reactive(as.integer(input$nGroups))
-    output$sampleGroups = renderUI({
-      nGroups = inputNumberGroups()
-      data = inputData()
-      colSampleNames = grep('sig', colnames(data))
-      sampleNames = colnames(data)[colSampleNames]
-      lapply (1:nGroups, function(i) {
-        checkboxGroupInput(inputId = paste0("Group", i), label = paste("Group", i),
-                           choiceNames = as.list(sampleNames), choiceValues = as.list(sampleNames))
-      })
+  ######################################################
+  ## Unsupervised analysis, i.e. explorative analysis ##
+  ######################################################
+  ## PCA plot
+  output$pcaPlot = renderPlot({
+    reactive(input$inputFile, {
+      ## Data reduction
+      rawData = inputData()$data
+      level = inputData()$level
+      if (level == "peptide") {
+        entry = rawData[, 1]
+      } else if (level == "protein") {
+        entry = rawData[, 2]
+      }
+      ## Definition of a dataset (only includes TMT reporter intensities)
+      ## Hereafter, the data is log2-transformed
+      colInd = grep("sig", colnames(rawData))
+      data = log(rawData[, colInd], 2)
+      rownames(data) = entry
+      
+      ## Preparation of PCA result for visualization
+      resPCA = prcomp(t(data), center = TRUE, scale = TRUE)
+      eigs = resPCA$sdev ^ 2
+      resPCA = data.frame(resPCA$x[, 1:2])
+      
+      ## Parameter setup for ggplot
+      xlabPCA = paste0("PC1 (", round((eigs[1] / sum(eigs)) * 100, 2),"%)")
+      ylabPCA = paste0("PC2 (", round((eigs[2] / sum(eigs)) * 100, 2),"%)")
+      ratioDisplay = 4/3
+      ratioValue = (max(resPCA$PC1) - min(resPCA$PC1)) / (max(resPCA$PC2) - min(resPCA$PC2))
+      print(ggplot(data = resPCA[, 1:2], aes(PC1, PC2)) +
+              geom_jitter(size = 3) +
+              geom_text(aes(label = rownames(resPCA)), vjust = "inward", hjust = "inward") +
+              labs(x = xlabPCA, y = ylabPCA) + 
+              coord_fixed(ratioValue / ratioDisplay))
     })
   })
-  
-  # Comparison between groups specified by the user input
-  DE = eventReactive(input$submitGroups, {
-    ## Load the data
-    data = inputData()
-    nGroups = inputNumberGroups()
-    
-    ## Get the information of samples to be compared
-    comparison = as.character()
-    for (g in 1:nGroups) {
-      groupName = paste0("Group", g)
-      comparison[g] = paste(input[[groupName]], collapse = ",")
-    }
-    groups = list()
-    nSamples = 0
-    compSamples = NULL
-    for (g in 1:nGroups) {
-      groups[[g]] = unlist(strsplit(comparison[g], ","))
-      nSamples = nSamples + length(groups[[g]])
-      compSamples = c(compSamples, groups[[g]])
-    }
-    
-    ## Generate a design matrix (which contains the information of comparison)
-    subColNames = colnames(data)[which(colnames(data) %in% compSamples)]
-    design = matrix(0, nrow = nSamples, ncol = nGroups)
-    for (g in 1:nGroups) {
-      design[which(subColNames %in% groups[[g]]), g] = 1
-    }
-    colnames(design) = paste("group", seq(1, nGroups), sep = "")
 
-    ## Generate a contrast matrix and new column names for the LIMMA result table
-    contVec = NULL
-    newColNames = NULL
-    combMatrix = combn(seq(1, nGroups), 2)
-    for (j in 1:ncol(combMatrix)) {
-      contVec = c(contVec, paste(paste("group", combMatrix[1, j], sep = ""), paste("group", combMatrix[2, j], sep = ""), sep = "-"))
-      newColNames = c(newColNames, paste(comparison[combMatrix[1, j]], "/", comparison[combMatrix[2, j]], sep = ""))
-    }
-    contMatrix = makeContrasts(contrasts = contVec, levels = design)
-  })
+  
+  
+  
+  
+  
+    
+  ## Heatmap of data (with hierarchical clustering)
+  
+
+  
+  # ################################################################
+  # ## Supervised analysis, i.e. differential expression analysis ##
+  # ################################################################
+  # ## Specificiation of sample groups
+  # inputNumberGroups = reactive(as.integer(input$nGroups))
+  # observeEvent(input$inputFile, {
+  #     output$sampleGroups = renderUI({
+  #     nGroups = inputNumberGroups()
+  #     data = inputData()
+  #     colSampleNames = grep('sig', colnames(data))
+  #     sampleNames = colnames(data)[colSampleNames]
+  #     lapply (1:nGroups, function(i) {
+  #       checkboxGroupInput(inputId = paste0("Group", i), label = paste("Group", i),
+  #                          choiceNames = as.list(sampleNames), choiceValues = as.list(sampleNames))
+  #     })
+  #   })
+  # })
+  # 
+  # ## Differentially expressed peptides/proteins
+  # DE = eventReactive(input$submitGroups, {
+  #   inputFile = input$inputFile$name
+  #   level = NULL
+  #   if (length(grep("pep", inputFile))) {
+  #     level = "peptide"
+  #   } else if (length(grep("pro", inputFile))) {
+  #     level = "protein"
+  #   }
+  #   data = inputData()
+  #   nGroups = inputNumberGroups()
+  #   comparison = as.character()
+  #   compSamples = as.character()
+  #   for (g in 1:nGroups) {
+  #     groupName = paste0("Group", g)
+  #     comparison[g] = paste(input[[groupName]], collapse = ",")
+  #   }
+  #   groups = list()
+  #   compSamples = NULL
+  #   for (g in 1:nGroups) {
+  #     groups[[g]] = unlist(strsplit(comparison[g], ","))
+  #     compSamples = c(compSamples, groups[[g]])
+  #   }
+  #   res = statTest(data, level, comparison)
+  # })
+  # 
+  # ## Volcano plot
+  # 
+  # ## Heatmap
+  # 
+  # ## DEpeptide/protein table
+  # output$DE = renderTable({
+  #   head(DE())
+  # })
+  
+  # DE = eventReactive(input$submitGroups, {
+  #   ## Load the data
+  #   data = inputData()
+  #   nGroups = inputNumberGroups()
+  # 
+  #   ## Get the information of samples to be compared
+  #   comparison = as.character()
+  #   for (g in 1:nGroups) {
+  #     groupName = paste0("Group", g)
+  #     comparison[g] = paste(input[[groupName]], collapse = ",")
+  #   }
+  #   groups = list()
+  #   nSamples = 0
+  #   compSamples = NULL
+  #   for (g in 1:nGroups) {
+  #     groups[[g]] = unlist(strsplit(comparison[g], ","))
+  #     nSamples = nSamples + length(groups[[g]])
+  #     compSamples = c(compSamples, groups[[g]])
+  #   }
+  # 
+  #   ## Generate a design matrix (which contains the information of comparison)
+  #   subColNames = colnames(data)[which(colnames(data) %in% compSamples)]
+  #   design = matrix(0, nrow = nSamples, ncol = nGroups)
+  #   for (g in 1:nGroups) {
+  #     design[which(subColNames %in% groups[[g]]), g] = 1
+  #   }
+  #   colnames(design) = paste("group", seq(1, nGroups), sep = "")
+  # 
+  #   ## Generate a contrast matrix and new column names for the LIMMA result table
+  #   contVec = NULL
+  #   newColNames = NULL
+  #   combMatrix = combn(seq(1, nGroups), 2)
+  #   for (j in 1:ncol(combMatrix)) {
+  #     contVec = c(contVec, paste(paste("group", combMatrix[1, j], sep = ""), paste("group", combMatrix[2, j], sep = ""), sep = "-"))
+  #     newColNames = c(newColNames, paste(comparison[combMatrix[1, j]], "/", comparison[combMatrix[2, j]], sep = ""))
+  #   }
+  #   contMatrix = makeContrasts(contrasts = contVec, levels = design)
+  # })
   
   # output$selectedSamples = renderText({
   #   groupName = paste0("Group", 1)
@@ -79,4 +167,8 @@ function (input, output) {
   #   paste("You chose", icons)
   # })
 
+  
+  output$table = renderTable(head(inputData()))
+  
+  
 }
--- ui.R
+++ ui.R
@@ -1,28 +1,34 @@
 ## ui.R
-
 fluidPage(
-  pageWithSidebar(
-    headerPanel("Analaysis and visualization of JUMP quantification results"),
-    sidebarPanel(
-      h2("About"),
-      p("This tool draws a volcano plot- typically used for showing relationships between fold change and p-values of genes (probes) in microarray studies.",
-        "Points are sized according to their X-value, and colour intensity is by their Y-value"), 
-      p("Which genes to labeSl are chosen based on Euclidean distance from the origin, but can be biased towards the X or Y dimension. All other settings should be self explanatory.",
-        "Editing any settings will cause the plot to be redrawn, label positions are fixed above the point currently."),
-      fileInput("inputFile", "Choose File",
-                accept=c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
-      numericInput("nGroups", "Number of groups in samples", value = 2, min = 2)
-    ),
-      
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Select samples for each group", 
-                uiOutput("sampleGroups"), 
-                actionButton("submitGroups", "Update")),
-        tabPanel("Volcano Plot", plotOutput(outputId = 'volcano', width = "800px", height = "800px")),
-        tabPanel("Histogram", plotOutput(outputId = 'histogram', width = "100%")),
-        tabPanel("Data Preview", tableOutput("contents"))
-      )
+  headerPanel("Analysis and visualization of JUMP quantification results"),
+  sidebarPanel(
+    h2("About"),
+    p("This tool provides exploratory data analysis and differential expression analysis results of a  proteomics dataset."),
+    p("Which genes to labeSl are chosen based on Euclidean distance from the origin, but can be biased towards the X or Y dimension. All other settings should be self explanatory.",
+      "Editing any settings will cause the plot to be redrawn, label positions are fixed above the point currently."),
+    fileInput("inputFile", "Choose File",
+              accept=c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
+    # numericInput("nGroups", "Number of groups in samples", value = 2, min = 2),
+    width = 3
+  ),
+  
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Exploratory analysis",
+               sidebarPanel(
+                 p("This panel provides explorative data analysis results for your proteomics dataset in three ways."),
+                 p("1. PCA (principal component analysis) plot of samples"),
+                 p("2. Hierarchical clustering result of samples and peptides/proteins"),
+                 p("3. MA plot showing the similarity between any two samples")
+               ),
+               mainPanel(plotOutput("pcaPlot"))),
+      tabPanel("Select samples for each group",
+               uiOutput("sampleGroups"),
+               actionButton("submitGroups", "Update"),
+               uiOutput("DE")),
+      tabPanel("Volcano Plot", plotOutput(outputId = 'volcano', width = "800px", height = "800px")),
+      tabPanel("Histogram", plotOutput(outputId = 'histogram', width = "100%")),
+      tabPanel("Data Preview", tableOutput("contents"))
     )
   )
 )
